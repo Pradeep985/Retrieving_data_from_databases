afe8b9bc4ad41b1146de597216c297b10c645982
pradeep985 <pradeepyadav24th@gmail.com>
2025-02-16 23:18:25 +0530

    update delete

--- models/product.js
+++ models/product.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
+
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
