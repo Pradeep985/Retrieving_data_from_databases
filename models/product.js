80196be16940f237223f25480d3bb8b47521d540
pradeep985 <pradeepyadav24th@gmail.com>
2025-02-16 21:46:34 +0530

    Sequelize

--- models/product.js
+++ models/product.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
-
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
